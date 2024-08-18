ac28ff1e5bfa7abddbf00b0b6c345f0e29cd429e
hokomili <hokomili@gmail.com>
2024-08-18 20:40:04 +0800

    init

---
+++ Assets/Script/Snake.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public static Snake instance;
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
