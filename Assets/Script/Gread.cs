36bb45397d17cbcbc2f75b4a9a79a2fe0df20a51
hokomili <hokomili@gmail.com>
2024-08-22 00:00:01 +0800

    gread system

---
+++ Assets/Script/Gread.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gread : MonoBehaviour
+{
+    public int x;
+    public int y;
+    public Gread left;
+    public Gread right;
+    public Gread up;
+    public Gread down;
+}
---
+++ Assets/Script/SnakeBody.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeBody : MonoBehaviour,ICollidible
+{
+    public void Collide(ICollidible collider)
+    {
+        Debug.Log("Collide!!!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
